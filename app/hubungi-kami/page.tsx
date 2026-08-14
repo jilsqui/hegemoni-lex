8036f9722d5b6cee9353dbcf5f65b51cb38d357a
hegemonilex <lexhegemoni@gmail.com>
2026-08-14 08:26:02 +0800

    fix: update email hubungi kami ke lexhegemoni@gmail.com

--- app/hubungi-kami/page.tsx
+++ app/hubungi-kami/page.tsx
@@ -25,7 +25,7 @@ export default function HubungiKamiPage() {
         </div>
 
         <div className="mt-8 md:mt-12">
-            <a href="mailto:redaksi@hegemonilex.id" className="bg-white text-black px-6 py-3 md:px-8 md:py-4 font-bold uppercase tracking-widest text-xs hover:bg-gray-200 transition-colors inline-block active:bg-gray-300">
+            <a href="mailto:lexhegemoni@gmail.com" className="bg-white text-black px-6 py-3 md:px-8 md:py-4 font-bold uppercase tracking-widest text-xs hover:bg-gray-200 transition-colors inline-block active:bg-gray-300">
                Hubungi Via Email ✉️
             </a>
         </div>
